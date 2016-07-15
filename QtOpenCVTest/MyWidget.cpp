335e4c3adb204b4096e03ac530483af53904ef15
MrChenChen <chenchenbbk@hotmail.com>
2016-07-15 15:33:06 +0800

    Add Init Color Add ts.bitmap

--- QtOpenCVTest/MyWidget.cpp
+++ QtOpenCVTest/MyWidget.cpp
@@ -29,10 +29,9 @@ void MyWidget::SetUpUI()
 
 		connect(button, &QPushButton::clicked, this, _fun);
 
-		button->setGeometry((m_ButtonCount % 8) * 100 + 10, int(m_ButtonCount == 0 ? 0 : log10(m_ButtonCount)) * 30 + 20, 80, 30);
+		button->setGeometry((m_ButtonCount % 8) * 100 + 10, int(m_ButtonCount / 8) * 40 + 20, 80, 30);
 
 		m_ButtonCount++;
-
 
 
 	};
@@ -47,6 +46,8 @@ void MyWidget::InitConnections()
 		Mat img = imread("1_0090_0.bmp", 0);
 
 		imshow("LoadImage", img);
+
+		namedWindow("LoadImage", CV_WINDOW_NORMAL);
 
 	});
 
@@ -116,7 +117,7 @@ void MyWidget::InitConnections()
 	});
 
 
-	AddButton("播放视频", [] 
+	AddButton("播放视频", []
 	{
 		VideoCapture video;
 
@@ -136,6 +137,103 @@ void MyWidget::InitConnections()
 		}
 
 	});
+
+
+	AddButton("创建PNG", []
+	{
+		Mat mat(480, 640, CV_8UC4);
+
+
+		for (size_t i = 0; i < mat.rows; i++)
+		{
+			for (size_t j = 0; j < mat.cols; j++)
+			{
+				auto & rgba = mat.at<Vec4b>(i, j);
+
+				rgba[0] = UCHAR_MAX;
+
+				rgba[1] = uchar(float(mat.cols - j) / float(mat.cols)*UCHAR_MAX);
+
+				rgba[2] = uchar(float(mat.rows - j) / float(mat.rows)*UCHAR_MAX);
+
+				rgba[3] = uchar(0.5*(rgba[1] + rgba[2]));
+
+			}
+		}
+
+		imshow("PNG", mat);
+
+		//imwrite("D:\\1.png", mat);
+
+	});
+
+
+	AddButton("图像混合", []
+	{
+		Mat img = imread("1_0090_0.bmp", 0);
+
+		imshow("1_0090_0.bmp", img);
+
+		Mat girl = imread("ts.bmp", 0);
+
+		imshow("ts.bmp", girl);
+
+		Mat imageROI = img(Rect(500, 120, girl.cols, girl.rows));
+
+		addWeighted(img(Rect(500, 120, girl.cols, girl.rows)), 0, girl, 1, 0.1, imageROI);
+
+		imshow("After", img);
+	});
+
+
+	AddButton("颜色图像", []
+	{
+		Mat red(300, 300, CV_8UC3, Scalar(0, 0, 255));
+
+		imshow("red", red);
+
+
+
+		Mat gray(300, 300, CV_8UC1, Scalar(128));
+
+		imshow("gray", gray);
+
+
+
+		Mat rndcolor(300, 300, CV_8UC3);
+
+		for (size_t i = 0; i < rndcolor.rows; i++)
+		{
+			for (size_t j = 0; j < rndcolor.cols; j++)
+			{
+				auto & t = rndcolor.at<Vec3b>(i, j);
+
+				t[0] = qrand() % 255;
+				t[1] = qrand() % 255;
+				t[2] = qrand() % 255;
+			}
+		}
+
+		imshow("rndcolor", rndcolor);
+
+
+
+		Mat rndgray(300, 300, CV_8UC1);
+
+		for (size_t i = 0; i < rndgray.rows; i++)
+		{
+			for (size_t j = 0; j < rndgray.cols; j++)
+			{
+				rndgray.at<uchar>(i, j) = qrand();
+			}
+		}
+
+		imshow("rndgray", rndgray);
+
+
+
+	});
+
 
 
 }
